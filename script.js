825ae17fbb6d3628fdb467b62d0b0b5988f5da88
nayeemhoque109 <mhoqu001@gold.ac.uk>
2025-02-14 17:53:10 +0000

    nayeem website

---
+++ script.js
@@ -0,0 +1,11 @@
+/*------JC for pop up menu-----*/
+
+    
+var menu = document.getElementById("menu");
+function showMenu(){
+    menu.style.right = "0";
+}
+function menuExit(){
+    menu.style.right = "-199px";
+}
+    
